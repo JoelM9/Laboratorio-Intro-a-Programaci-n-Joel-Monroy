1d09c71d8afe6677b66da30a0564b2179ee7adb5
JoelM9 <113248095+JoelM9@users.noreply.github.com>
2022-09-26 10:58:08 -0600

    Add files via upload

---
+++ L1_Joel Monroy_1043721/L1_Joel Monroy_1043721/L1_Joel Monroy_1043721/L1_Joel Monroy_1043721/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace L1_Joel_Monroy_1043721
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nombre;
+            Console.WriteLine("Escriba su nombre");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("Soy " +nombre);
+
+            Console.ReadKey();
+
+            Console.Write("Hola Mundo Soy "+ nombre);
+            Console.ReadKey();
+        }
+    }
+}
