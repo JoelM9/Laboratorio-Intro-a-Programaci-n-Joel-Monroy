6fb6306faabf7c8de9b36b1f26f9a749b7a545ba
JoelM9 <113248095+JoelM9@users.noreply.github.com>
2022-10-08 10:17:51 -0600

    Add files via upload

---
+++ T7_JEMH_1043721/T7_JEMH_1043721/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace T7_JEMH_1043721
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            double i=1, C=0;
+
+            Console.WriteLine("escribir el numero");
+            N = int.Parse(Console.ReadLine());
+
+            do
+            {
+                C = C + (1 / i);
+                i++;
+
+            } while (N >= i);
+            Console.WriteLine("La primer sumatoria es: " + C);
+
+            double b = 0;
+            double j = 1;
+            do
+            {
+                b = b + (1 / Math.Pow(2, j));
+                j++;
+
+            } while (N >= j);
+            Console.WriteLine("La segunda sumatoria es: " + b);
+
+            Console.WriteLine(" Ingresar el valor de X");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine(" Ingresar el valor de a");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine(" Ingresar el valor de n");
+            int n = int.Parse(Console.ReadLine());
+
+            int y = n - 0;
+            double f = 1 * Math.Pow(a, y);
+
+            Console.WriteLine("El resultado es: "+f);
+        }
+    }
+}
