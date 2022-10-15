ee63d7160b8bbf1420439d44b825bf213ccdac66
JoelM9 <113248095+JoelM9@users.noreply.github.com>
2022-10-15 09:35:47 -0600

    Add files via upload

---
+++ L10_JEMH_1043721/L10_JEMH_1043721/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L10_JEMH_1043721
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ L10_JEMH_1043721/L10_JEMH_1043721/TRIANGULORECTANGULO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L10_JEMH_1043721
+{
+    class TRIANGULO_RECTANGULO
+    {
+        public double CATETOA;
+        public double anguloOpuestoA;
+
+        public double ObtenercatetoB()
+        {
+            return Math.Round(CATETOA / Math.Tan(anguloOpuestoA),3);
+        }
+
+        public double obtenerhhipotenusa()
+        {
+            return Math.Round(CATETOA / Math.Sin(anguloOpuestoA),3);
+        }
+
+        public double obteneranguloOpuestoB()
+        {
+            return Math.Round( 90 - anguloOpuestoA,3);
+        }
+
+        public double obtener()
+        {
+
+        }
+    }
+}
