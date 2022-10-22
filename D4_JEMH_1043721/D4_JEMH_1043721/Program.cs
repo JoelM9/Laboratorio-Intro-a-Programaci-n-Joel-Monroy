f93bf59d7f67165984c40a9eab9110198982866d
JoelM9 <113248095+JoelM9@users.noreply.github.com>
2022-10-22 08:45:35 -0600

    Semana 11

---
+++ D4_JEMH_1043721/D4_JEMH_1043721/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace D4_JEMH_1043721
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] billetes = new int[6];
+            billetes[0] = 100;
+            billetes[1] = 50;
+            billetes[2] = 20;
+            billetes[3] = 10;
+            billetes[4] = 5;
+            billetes[6] = 1;
+            Console.WriteLine("Ingresar una cantidad: Q");
+            int cantidad = int.Parse(Console.ReadLine());
+
+           for(int i=0; i<billetes.Length; i++)
+            {
+                int x = cantidad / billetes[i];
+                cantidad = cantidad % billetes[i];
+                Console.WriteLine("Q" + billetes[i] + " = " + x);
+            }
+        }
+    }
+}
